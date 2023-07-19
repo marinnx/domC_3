2e25fd93fa12107bbac9043848f648173e3ff574
marinnx <138767116+marinnx@users.noreply.github.com>
2023-07-19 18:56:36 +0900

    Add files via upload

---
+++ task23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int GetData(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+void GetKub(int limit)
+{
+    for (int i = 1; i < limit; i++)
+    {
+        Console.Write($"{i*i*i}, ");
+    }
+    Console.Write(limit*limit*limit);
+}
+int num = GetData("Введите число N ");
+GetKub(num);
