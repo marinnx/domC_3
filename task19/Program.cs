c2295bd5a7a06484c01c04f4787e6c0c4905b033
marinnx <138767116+marinnx@users.noreply.github.com>
2023-07-19 18:55:01 +0900

    Add files via upload

---
+++ task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+int GetData(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+string TestNumber(int number)
+{string otvet = "";
+    if (number / 10000 == number %10 && number / 1000 % 10 == number / 10 % 10)
+    {
+        otvet = "да";
+    }
+    else
+    {
+        otvet = "нет";
+    }
+    return otvet;
+}
+int num = GetData("Введите пятизначное число: ");
+Console.WriteLine($"{num} -> {TestNumber(num)}");
